a3bd2737715cad9b33aec423ba44a4f28cae372e
Sudhakar <sudhakars3492@gmail.com>
2026-02-03 17:34:20 +0530

    changes in the gps

--- rvce-campus-map/src/data/gps.js
+++ rvce-campus-map/src/data/gps.js
@@ -10,8 +10,8 @@
  */
 export const MAP_BOUNDS = {
   topLeft: {
-    lat: 12.925619269693883, // Replace with actual Top-Left Latitude
-    lon: 77.49714811319096   // Replace with actual Top-Left Longitude
+    lat: 12.925487667286825, // Replace with actual Top-Left Latitude
+    lon: 77.49752010566218   // Replace with actual Top-Left Longitude
   },
   bottomRight: {
     lat: 12.922083397596785, // Replace with actual Bottom-Right Latitude
